d8b8b05b7a279b10988491d7adc92545a12713fa
cc <44751363+zzfn@users.noreply.github.com>
2022-10-01 11:07:50 +0800

    :sparkles:

--- linux.sh
+++ linux.sh
@@ -40,15 +40,11 @@ install-nodejs() {
         echo "* Installing NVM..."
         echo "-----------------------------------------------------------"
 
-        curl -o- https://cdn.jsdelivr.net/gh/creationix/nvm@v0.33.11/install.sh | bash
-
-        export NVM_DIR="$HOME/.nvm"
-        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-        [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
+        curl https://get.volta.sh | bash
+        volta install node
         echo "-----------------------------------------------------------"
         echo -n "* NVM Verision: "
-        command -v nvm
+        command -v node
     }
 
     install-node() {
